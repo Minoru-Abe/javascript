b9d338ee7dc5715927e7cc4cefd0f516f61fb690
Minoru-Abe <tomominoru.abe@gmail.com>
2024-10-26 06:04:18 +0900

    spread2

--- 02/index.js
+++ 02/index.js
@@ -172,10 +172,35 @@
 // };
 // console.log(myProfile);
 
-const arr1 = [1,2];
-console.log(arr1);
-console.log(...arr1);
+// スプレッド構文
+// const arr1 = [1,2];
+// console.log(arr1);
+// console.log(...arr1);
 
-const sumfunc = (num1, num2) => console.log(num1 + num2);
-sumfunc(1,2);
-sumfunc(...arr1);
+// const sumfunc = (num1, num2) => console.log(num1 + num2);
+// sumfunc(1,2);
+// sumfunc(...arr1);
+
+
+// // まとめる使い方
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+arr4[0] = 99;
+console.log(arr4);
+console.log(arr6);
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
+
+
