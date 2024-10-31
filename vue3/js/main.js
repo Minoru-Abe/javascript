da919f4bb9874dfb174f494a66ccd6c63b64927a
Minoru-Abe <tomominoru.abe@gmail.com>
2024-10-31 23:15:38 +0900

    practice2

--- vue3/js/main.js
+++ vue3/js/main.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
     data: () => ({
-        message: 'Hello!!'
+        message: 'Hello'
     })
 })
 app.mount('#app')
+
+const app2 = Vue.createApp({
+    data: () => ({
+        message: 'Hello2'
+    })
+})
+app2.mount('#app2')
