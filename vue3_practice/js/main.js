2938a4b4ce17c6c4be618dfa893772159b19a0c2
Minoru-Abe <aberusmail0180@gmail.com>
2024-11-02 16:03:39 +0900

    anyway finshed simple todo lists

--- vue3_practice/js/main.js
+++ vue3_practice/js/main.js
@@ -5,11 +5,17 @@ const app = Vue.createApp({
     }),
     methods: {
         addItem(event) {
-            console.log("clicked")
+            // console.log("clicked")
+            if (this.newItem === "") return
             let todo = {
-                item: this.newItem
+                item: this.newItem,
+                isDone: false
             }
             this.todos.push(todo)
+            this.newItem = ""
+        },
+        deleteItem(index) {
+            this.todos.splice(index, 1)
         }
     }
 })
