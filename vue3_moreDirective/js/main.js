e92ec79bd8076985a387c5d5d4720e290f4fc08e
Minoru-Abe <tomominoru.abe@gmail.com>
2024-11-04 06:46:55 +0900

    computed and method

--- vue3_moreDirective/js/main.js
+++ vue3_moreDirective/js/main.js
@@ -6,6 +6,17 @@ const app = Vue.createApp({
         reversedMessage: function(){
             return this.message.split('').reverse().join('')
 
+        },
+        add: function() {
+            return Math.random()
+        }
+    },
+    methods: {
+        reversedMessageMethod: function() {
+            return this.message.split('').reverse().join('')
+        },
+        addMethod: function() {
+            return Math.random()
         }
     }
 })
