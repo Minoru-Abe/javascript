c64e2ceb0887b5a056d8059b2ad4d3310ff05da6
Minoru-Abe <tomominoru.abe@gmail.com>
2024-11-06 23:05:26 +0900

    api finished

--- vue3_api/js/main.js
+++ vue3_api/js/main.js
@@ -1,17 +1,45 @@
 const app = Vue.createApp({
     data: ()=> ({
+        results: null,
         searchText: '',
-        results: []
+        message: ''
     }),
     watch: {
-        searchText: function(newValue, oldValue){
-            clearTimeout(this.inputTimeout)
-            this.inputTimeout = setTimeout( () => {
-                this.results.push({name: oldValue})
-                console.log(this.results)
-            }, 500)
+        searchText: function(newKeyword, oldKeyword){
+            console.log(newKeyword)
+            this.message = 'Waiting for you to stop typing'
+            this.debouncedGetAnswer()
+        }
+    },
+    mounted: function() {
+        // this.searchText = 'javascript'
+        // this.getAnswer()
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+    },
+    methods: {
+        getAnswer: function(){
+            if (this.searchText === '') {
+                console.log('karamoji')
+                this.results = null
+                return
+            }
+            this.message = 'loading...'
+            const vm = this
+            const params = {page: 1, per_page: 20, query: this.searchText}
+            axios.get('https://qiita.com/api/v2/items', { params })
+                .then(function(response){
+                    vm.results = response.data
+                    console.log(results)
+                })
+                .catch(function(error){
+                    vm.message = 'Error!' + error
+                })
+                .finally(function() {
+                    vm.message = ''
+                })
 
         }
     }
+
 })
 app.mount('#app')
