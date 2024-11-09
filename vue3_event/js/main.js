ebd505120195784df2d47263999685037e9bafa6
Minoru-Abe <aberusmail0180@gmail.com>
2024-11-09 14:51:15 +0900

    event handler

--- vue3_event/js/main.js
+++ vue3_event/js/main.js
@@ -1,24 +1,10 @@
 const app = Vue.createApp({
     data: () => ({
-        counter: 0,
         message: ''
     }),
     methods: {
-        countUp: function(event) {
-            if (event.target.id === 'click1'){
-                this.counter++
-            }
-            else if (event.target.id === 'click2'){
-                this.counter--
-            }
-            else if (event.target.id === 'click3'){
-                this.counter = this.counter * 2
-            }
-            console.log(event.target.id)
-        },
-        clickHandler: function($event, message) {
-            this.message = message
-            console.log($event)
+        clickHandler: function(){
+            this.message = new Date().toLocaleTimeString()
         }
     }
 })
