ff86dd824bd7a5fac8c1724d30454044893510b6
Minoru-Abe <aberusmail0180@gmail.com>
2024-11-10 12:12:33 +0900

    local component

--- vue3_component/js/main.js
+++ vue3_component/js/main.js
@@ -1,12 +1,16 @@
+const helloComponent = {
+    template:'<p>Hello!</p>'
+}
+
 const app = Vue.createApp({
     data: () => ({
         message: 'Hello Vue.js!'
-    })
+    }),
+    components: {
+        'hello-component': helloComponent
+    }
 })
 
-app.component('hello-component', {
-    template: '<p>Hello!</p>'
-})
 
 app.component('goodnight-component', {
     template: '<p>goodnight!</p>'
