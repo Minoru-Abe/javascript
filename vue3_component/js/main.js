0887c90c92de97cfd6b70a2a9fdc3c007cabc5c4
Minoru-Abe <aberusmail0180@gmail.com>
2024-11-10 12:30:06 +0900

    button counter with component

--- vue3_component/js/main.js
+++ vue3_component/js/main.js
@@ -2,14 +2,30 @@
 //     template:'<p>Hello!</p>'
 // }
 
+const total = 0
+
+const buttonCounter = {
+    template: '<div><span>count:</span><button v-on:click="countUp">{{count}}</button></div>',
+    data: () => ({
+        count: 0
+    }),
+    methods: {
+        countUp: function(event){
+            this.count++
+        }
+    }
+}
+
 const app = Vue.createApp({
     data: () => ({
-        message: 'Hello Vue.js!'
+        message: 'Hello Vue.js!',
+        totalCount: total
     }),
     components: {
         'hello-component': {
             template: '<p>Hello!</p>'
-        }   
+        },
+        'counter-component': buttonCounter   
     }
 })
 
